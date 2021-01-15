583b16c13a59dfa5822782222042f8bcf3ea328d
lDarkHawkI <lbraini@icloud.com>
2021-01-15 09:02:46 +0300

    X

---
+++ src/KeyValidator.java
@@ -0,0 +1,31 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+public class KeyValidator {
+    public static String[] keys = new String[]{"8LUSq4#Jtb>CW4Y", "+pD2Jf^8V+9Dy6a", "T+^Re8^Eg~BanYp", "M#PD2Akj4H~7?%D", "6+n9^SCW5$xFNS+", "BcEg!u&F2>D4^Xx", "dVMK2Yxr##6L8PZ", "Cr>mQkZgM$3LfsN", "MFYAW?867N#^4xu", "y^B#A@54+LM4a3G", "T3JxK^D!7SvFC+^", "7!~rsQ2R@FxNJZT", "mS$5n>jUES2&x2D", "G&R%A&@EnD&3~m2", "qLGA33NDcW?Ss8$", "+zWLAPBnQd?#t3e", "r@Hjche8X?z~9Xb", "cPJB7L9^n9NA4rH", "??zxC5@UGMK2WCy", "$Yn5bJ4QtQ3ua?S", "uU&zVJ~fX65DJr3", "bSA>h%Qt?edT2D7", "#6K3d4km8WqM^Av", "29t3FTbfP&LK7V@", "fZ43dyM>&j#3HZv", ">KWtSHQUJ5HwmT!", "~4^5!ZvBmRyh8~D", "S2ny?+3WxDjg!XS", "Fm5Ry4a5N%V^wLE", "+r6b#kFfUMX?%zG", "9!>+^tRU+e63nN5", "w9?6s^EkmVtdxPz", "P#TW%F5$j$Sufe2", "A25X>VUDw$JFDx&", "dUCZWwcyHS9mB>6", "$Uq>?jkzJ9KsP3v", "$N>ncQ84eKM2fX7", "3gDtP>Z%?#uhVRX", "XnKbE&tJQuNj!V@", "~2KV6q6Sf3sznKc", "w5E#HXde^uGAdzr", "kj7+UPEA6Z~F!Q5", "BHkDQt7zde4V>+E", "X#eDs226d~3HPDv", "tngw6ZPf~@V>XKZ", "p+UtD&Wy3LX7fAw", "EKG~nWar2Y6m25x", "9JfVFE>K>MyhqUe", "D^s7nP6Pf~x$>E&", "8TaDx!^yNv9XEc%", "~BAPX7gH++Es2e5", "4xCKFrw7G7AL@?D", "ub2g@>&Tjqt78E?", "d@NFK?xQU8#qY~6", "!Gta^TZC+Dx#d3R", "9Lj$>S?kh&Kf@g+", "Z#HXY6Q?j7s3nCx", "4hsTx^>c3V#@7BU", "MbA%PZN4hVGfh+3", "vBqK>f~MVDj7y98", "~T^#VDDLh6+s@99", "Z?Z6#+s6m^2CbyW", "~cXECp^F5S?5n~r", "W3c5xe5qRSgj>?3", "z9VE8sM5Za!$4H^", "9Ee!+3JySvkjBg6", "D65yU%Gpwd^#d?n", "nRz5PT!97?VvLe5", "MYndLGQ7eaRU%y8", "SM75gvG6sTR$CU@", "9~Kq$8sHDh#Ec7%", "QKfn4!dWMPUFEg3", "dxC@PFDk4tFD847", "+B2@P~K9FPmyM8p", "3XGULy>DuM9$2Am", "~L4RGU2P2WWn%S+", ">w9@@uLTaxS4Vjv", "#kaY2DpA43Fkdc6", "H&9cP4$>X4?^5mW", "wDHz~&geU@+t27A", "FPM+mH%AdCXA2r3", "pXUxgF$B76&@!~&", "sqJDz%df&C5R#g9", "%UxNv2SR@Z8E?nD", "LEqwWN#uv?JX5U+", "8K3LX+j4YBUED~^", "Z#6pretQM!s%C>L", "%&j42DP9BdAFy8B", "&!GLbcLR5kvz~KZ", "hD!v7DJn!CKZB5Q", "L%QbT+ad~dZJ8C6", "$!@xb4t>N>GC95V", "T%~2D7rCRPqEAHu", "u5b$BDzF5nHUc#C", "S#MAkTLn22ZE#bP", "U#3nTeudZD26>&H", "6yQrPuR8pBX^vq8", "tq63E$>64DUn#&X", "RdCnyB9#D^6EBE7", "q8Z&FNuPkWAW4~3", "DMtc4U!DhRYs9d5", "PDU4M5VBc&%zN%E", "d&!jsuUQ!+h9T5Z", "!Y%Cdcj7aLVpqa4", "Y5ZJYe&x@7>vEqs", "r6B&FSC4jSd!Xut", "@T%dn4B98dj!zPD", "s6USK~z3dvYfdMN", "2uWa8PLL!#3ktHx", "cL4A42#^3ZdVUR5", "W2JH%3z$k>xf?F+", "CWEa3RG6L?K8Hn3", "Az>#n@d9uHZECMt", "Z>Uwm9!KAGBz4f2", "tMWjz5!D@PECU35", "3FdRXU&f^KRrDEz", "4Bg5A#FGtc5Ce+b", "E$9ubG$@MYYW94T", "8p65XDL@FWregkQ", "?Bm$wUQY>~6f7VN", "9phH7FF~B#wYjSc", "28%jYYGdK?ZDw$E", "7jGSS3#M5E!CAYP", "^@6HZnNSG^6KC7u", "kxdDuV765rf+Gem", "$pS#+#ZHDz9M9Xc", "K4HC$3fASk!d>2d", "j8~p%R7gxhJ>d!!", "Wk?yE29bRJ+&wTQ", "HrSfMs6y7z@F+QL", "YPE3XS8VZRdvhKw", "?X4aJyFT+DkD2pb", "x%uH+cG7ngK>@z5", "4>eR@!gDAhRr6ap", "D3R7Ye3czaTs!tq", "G3Tvu&EU$93#A>+", "p+3dz^gBu3XvX#S", "^+Ne>h5X?Dk22V~", "%+Z4U!8E46Qs3BF", "gkV&9a&p!8%Du4d", "C~m+h?Nb5v9xJNA", "$RpFwBH9eMgBXh9", "L@%sh39U^Jthz#$", "A?$PXrS52>@6UVd", "8ELyRS5rZET3X7+", "$C^6YsR%ZPZLG9D", "M#8WJuzqHym4vd4", "B?tn9JZ7!83PfuR", "PUxC326B$dTn9Y%", ">2ZDDLxn9dAEt@W", "NW4S%p+Dc~YQzvJ", "CLmFkbD%6AnJydp", "U7#+M@HPSaAvHKd", "dN3hk7#L%QbHJjB", "R67v@SMu#hGT9%w", "v7qCL6~PgmTnZw%", "8DzKY@qGdCT7N5~", "HYRdPv#U7$4tkK+", "aLC83D^6u5Fr%#d", "ADXUhj8u!qK%ZeB", "&Dt8HVD!qDak@LQ", "JmQ7$H#TZ39s?eh", "tUefLXx+!Cw!k88", "BkM3V5~?Z7S4DE!", "Y8tm&UWgDPTb#Ge", "~nLtzT66f6JQKs8", "2#6LJDX&FCY^@33", "b9hDSm?$e+kJRGF", "yJ6$Y>2ANM#T#L7", "UWDrpbY?Gq@#TLd", "Gqyx2P!WMR9Hg+n", "DxK>d$#P9#f2v5@", ">+8#z9@Bg!DMM57", "832N5h@a8mD@HeF", "q%c>74r5sG%WTaY", "D2KRHwr$WE&V2zB", "T5aR#!2hGEU~uTN", "44$ap&5kYDC#hK@", "2d#VmG%Q5P@jMZR", "t9g$6VDdc7F~3MP", "dcBLT$e6kYKjs&4", "2prag!%hfwQ^5Mq", "cTs6LpJs+7@82>w", "5j?VW74dF8kZyZR", "DN^TM2dQd@rKFfv", "w%Zg9$y5S!w25#U", "k846A8%6K#FvQpZ", "3KD~S3jPU4jQK@^", "?K@#TPUM8GfQRbm", "M2c&LTP?$+jLwH4", "9X?zbv4D3E&Wp2V", "cYGR9s?5v2p6W~D", "7DVx5GF6pR@8+Nk", "G>fA>cF4!TDSzgn", "8SZPk^$3sRHn#5q", "JeHTux^NM>mzb6p", "^66z&mQ4A>wqn@T", "hr38Pt?sVFqPwH^", "4d?B2Zy8UH%E7u%", "n6X@dxT3su^vSKc"};
+
+    public KeyValidator() {
+    }
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("You must specify key (at least one)");
+        } else {
+            List<UUID> list = (List)Arrays.stream(keys).map((o) -> {
+                return UUID.nameUUIDFromBytes(o.getBytes(StandardCharsets.UTF_8));
+            }).collect(Collectors.toList());
+            Arrays.stream(args).map(UUID::fromString).forEachOrdered((o) -> {
+                System.out.println(String.format("Result for %s: %s", o.toString(), list.contains(o) ? "OK" : "FAIL"));
+            });
+        }
+    }
+}
+
